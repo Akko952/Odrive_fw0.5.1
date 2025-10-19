14d7c7d7a658bc3e7af2df1aecb1dafdf1ede21b
AkkO_0 <2232729734@qq.com>
2025-10-19 13:08:11 +0800

    feat: configure velocity control loop defaults

--- MotorControl/controller.hpp
+++ MotorControl/controller.hpp
@@ -19,15 +19,15 @@ public:
     } Anticogging_t;
 
     struct Config_t {
-        ControlMode control_mode = CONTROL_MODE_POSITION_CONTROL;  //see: ControlMode_t
-        InputMode input_mode = INPUT_MODE_PASSTHROUGH;             //see: InputMode_t
+        ControlMode control_mode = CONTROL_MODE_VELOCITY_CONTROL;  //see: ControlMode_t
+        InputMode input_mode = INPUT_MODE_VEL_RAMP;             //see: InputMode_t
         float pos_gain = 20.0f;                  // [(turn/s) / turn]
-        float vel_gain = 1.0f / 6.0f;            // [Nm/(turn/s)]
-        // float vel_gain = 0.2f / 200.0f,       // [Nm/(rad/s)] <sensorless example>
-        float vel_integrator_gain = 2.0f / 6.0f; // [Nm/(turn/s * s)]
-        float vel_limit = 2.0f;                  // [turn/s] Infinity to disable.
+        float vel_gain = 0.2f;            // [Nm/(turn/s)]
+        //float vel_gain = 0.2f / 200.0f;       // [Nm/(rad/s)] <sensorless example>
+        float vel_integrator_gain = 0.07f; // [Nm/(turn/s * s)]
+        float vel_limit = 30.0f;                  // [turn/s] Infinity to disable.
         float vel_limit_tolerance = 1.2f;        // ratio to vel_lim. Infinity to disable.
-        float vel_ramp_rate = 1.0f;              // [(turn/s) / s]
+        float vel_ramp_rate = 10.0f;              // [(turn/s) / s]
         float torque_ramp_rate = 0.01f;          // Nm / sec
         bool circular_setpoints = false;
         float circular_setpoint_range = 1.0f; // Circular range when circular_setpoints is true. [turn]
